291b736c360ae04c2767af209cf7cd18f6b920da
Nikolay <nickchirgin@gmail.com>
2023-01-31 21:31:28 +0300

    added authorization

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -13,8 +13,8 @@ FactoryBot.define do
     factory :manager do
       type { 'Manager' }
     end
-    factory :admin do
-      type { 'Admin' }
-    end
+    # factory :admin do
+    #   type { 'Admin' }
+    # end
   end
 end
