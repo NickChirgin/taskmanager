ae9e1e7f72e5626c3ce7ee1daefb2a0e8ac57de2
Nikolay <nickchirgin@gmail.com>
2023-01-31 17:46:11 +0300

    added basic layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import 'material-design-lite/material.js';
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
